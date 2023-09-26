d7571b0e819cdca3eb3c600106936caf171f6ba6
Khumo Letlape <31062011+sletlape@users.noreply.github.com>
2023-09-26 14:50:07 +0200

    P3: Testing search backend API

--- est-backend-node/test/employee.test.ts
+++ est-backend-node/test/employee.test.ts
@@ -49,4 +49,68 @@ describe('Employee API', () => {
         });
 
     });
+
+    describe('DELETE /api/employees/:id', () => {
+        it('should delete an employee by ID', async () => {
+            // Create a new employee
+            const newEmployee = {
+                firstName: "John",
+                lastName: "Doe",
+                // Add other employee properties as needed
+            };
+            const createResponse = await request(app).post('/api/employees').send(newEmployee);
+
+            // Delete the created employee by ID
+            const deleteResponse = await request(app).delete(`/api/employees/${createResponse.body.employee._id}`);
+
+            expect(deleteResponse.status).toBe(204);
+        });
+
+        it('should return a 404 status if the employee ID does not exist', async () => {
+            const nonExistentEmployeeId = 'non-existent-id';
+            const response = await request(app).delete(`/api/employees/${nonExistentEmployeeId}`);
+
+            expect(response.status).toBe(404);
+        });
+    });
+
+    describe('GET /api/employees/search', () => {
+        it('should search employees by first name, last name, or skill', async () => {
+            // Create employees with specific names and skills
+            const employee1 = {
+                firstName: "John",
+                lastName: "Doe",
+                skills: [{ skill: "Java" }],
+            };
+            const employee2 = {
+                firstName: "Jane",
+                lastName: "Smith",
+                skills: [{ skill: "Python" }],
+            };
+            const employee3 = {
+                firstName: "Alice",
+                lastName: "Johnson",
+                skills: [{ skill: "JavaScript" }],
+            };
+            await request(app).post('/api/employees').send(employee1);
+            await request(app).post('/api/employees').send(employee2);
+            await request(app).post('/api/employees').send(employee3);
+
+            // Search for employees with "John" in their name or "JavaScript" in their skills
+            const response = await request(app).get('/api/employees/search?search=John');
+
+            expect(response.status).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+            expect(response.body.length).toBe(2); // There should be 2 matching employees
+        });
+
+        it('should return an empty array if there are no matching employees', async () => {
+            // Search for an employee with a non-existent name
+            const response = await request(app).get('/api/employees/search?search=NonExistentName');
+
+            expect(response.status).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+            expect(response.body.length).toBe(0); // No matching employees
+        });
+    });
 });
